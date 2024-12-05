cff19e9a8526182b8716c68eaa0e458c34643ffe
wKobacki <woj.kobacki@gmail.com>
2024-12-05 13:35:02 +0100

    refactor

--- src/components/dashboard/Topbar.js
+++ src/components/dashboard/Topbar.js
@@ -4,22 +4,22 @@ import "../../styles/dashboard/Topbar.css"
 const Topbar = () => {
     const navigate = useNavigate();
     return (
-      <header className="topbar">
-        <div className="profile-section" onClick={() => navigate("/profile")}>
-          <img
-            src="https://via.placeholder.com/40"
-            alt="Profil"
-            className="profile-picture"
-          />
-          <span className="profile-name">Twój Profil</span>
-        </div>
-        <button
-          className="settings-button"
-          onClick={() => navigate("/settings")} 
-        >
-          ⚙️
-        </button>
-      </header>
+        <header className="topbar">
+            <div className="profile-section" onClick={() => navigate("/profile")}>
+                <img
+                    src="https://via.placeholder.com/40"
+                    alt="Profil"
+                    className="profile-picture"
+                />
+                <span className="profile-name">Twój Profil</span>
+            </div>
+            <button
+                className="settings-button"
+                onClick={() => navigate("/settings")}
+            >
+                ⚙️
+            </button>
+        </header>
     );
 }
 
