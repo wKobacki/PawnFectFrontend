9c197441384c8b8237e791639d9aef624df09153
Wegles <kuba2255kuba@gmail.com>
2024-12-12 00:38:57 +0100

    Poprawiony wyglad lekko

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -34,98 +34,56 @@ function HomePage() {
         scrolled ? "scrolled" : ""
       }`}
     >
-      <div
-        className={`home-container ${darkMode ? "dark" : ""} ${
-          scrolled ? "scrolled" : ""
-        }`}
-      >
-        <nav className="navbar">
-          <div className="nav-left">
-            <button onClick={toggleTheme} className="theme-toggle-button">
-              {darkMode ? <FaMoon /> : <FaSun />}
-            </button>
-          </div>
-          <div className="nav-center">
-            <div className="nav-title">PawFect</div>
-          </div>
-          <div className="nav-right">
-            <Link to="/login" className="nav-button">
-              Zaloguj się
-            </Link>
-            <Link to="/register" className="nav-button">
-              Zarejestruj się
-            </Link>
-          </div>
-        </nav>
-
-        <div className="main-content">
-          <div className="main-message">
-            Aby twoim zwierzakom żyło się lepiej
-          </div>
-
-          <section className="section dog-section">
-            <div className="section-content">
-              <img src={dogImage} alt="Pies" className="section-image-dog" />
-              <div className="section-text-dog">
-                <h2>Bruno</h2>
-                <p>Wiek: 5 lat</p>
-                <p>Rasa: Labrador</p>
-                <p>Barwa: Złoty</p>
-                <p>Dieta: Sucha karma</p>
-                <p>Uczulenia: Brak</p>
-              </div>
-            </div>
-          </section>
-          <section className="section dog-section">
-            <div className="section-content">
-              <img src={dogImage} alt="Pies" className="section-image-dog" />
-              <div className="section-text-dog">
-                <h2>Bruno</h2>
-                <p>Wiek: 5 lat</p>
-                <p>Rasa: Labrador</p>
-                <p>Barwa: Złoty</p>
-                <p>Dieta: Sucha karma</p>
-                <p>Uczulenia: Brak</p>
-              </div>
-            </div>
-          </section>
-
-          <section className="section cat-section">
-            <div className="section-content">
-              <img src={catImage} alt="Kot" className="section-image-cat" />
-              <div className="section-text-cat">
-                <h2>Tygrys</h2>
-                <p>Wiek: 3 lat</p>
-                <p>Rasa: Dachowiec</p>
-                <p>Barwa: Jasno-brązowa panterka</p>
-                <p>Dieta: Mokre saszetki</p>
-                <p>Uczulenia: Cytrusy</p>
-              </div>
-            </div>
-          </section>
-          <section className="section cat-section">
-            <div className="section-content">
-              <img src={catImage} alt="Kot" className="section-image-cat" />
-              <div className="section-text-cat">
-                <h2>Tygrys</h2>
-                <p>Wiek: 3 lat</p>
-                <p>Rasa: Dachowiec</p>
-                <p>Barwa: Jasno-brązowa panterka</p>
-                <p>Dieta: Mokre saszetki</p>
-                <p>Uczulenia: Cytrusy</p>
-              </div>
-            </div>
-          </section>
-
-          <section className="about-section">
-            <h2>O aplikacji</h2>
-            <p>
-              Nasza aplikacja pomaga właścicielom zwierząt domowych w
-              zarządzaniu informacjami o ich pupilach. Dzięki intuicyjnemu
-              interfejsowi, wszystkie informacje są zawsze pod ręką.
-            </p>
-          </section>
+      <nav className="navbar">
+        <div className="nav-left">
+          <button onClick={toggleTheme} className="theme-toggle-button">
+            {darkMode ? <FaMoon /> : <FaSun />}
+          </button>
         </div>
+        <div className="nav-center">
+          <div className="nav-title">PawFect</div>
+        </div>
+        <div className="nav-right">
+          <Link to="/login" className="nav-button">
+            Zaloguj się
+          </Link>
+          <Link to="/register" className="nav-button">
+            Zarejestruj się
+          </Link>
+        </div>
+      </nav>
+
+      <div className="main-content-homepage">
+        <div className="main-message">Aby twoim zwierzakom żyło się lepiej</div>
+
+        <section className="section dog-section">
+          <div className="section-content">
+            <img src={dogImage} alt="Pies" className="section-image-dog" />
+            <div className="section-text-dog">
+              <h2>Bruno</h2>
+              <p>Wiek: 5 lat</p>
+              <p>Rasa: Labrador</p>
+              <p>Barwa: Złoty</p>
+              <p>Dieta: Sucha karma</p>
+              <p>Uczulenia: Brak</p>
+            </div>
+          </div>
+        </section>
+
+        <section className="section cat-section">
+          <div className="section-content">
+            <img src={catImage} alt="Kot" className="section-image-cat" />
+            <div className="section-text-cat">
+              <h2>Tygrys</h2>
+              <p>Wiek: 3 lat</p>
+              <p>Rasa: Dachowiec</p>
+              <p>Barwa: Jasno-brązowa panterka</p>
+              <p>Dieta: Mokre saszetki</p>
+              <p>Uczulenia: Cytrusy</p>
+            </div>
+          </div>
+        </section>
+
         <section className="about-section">
           <h2>O aplikacji</h2>
           <p>
--- src/components/dashboard/AnimalCard.js
+++ src/components/dashboard/AnimalCard.js
@@ -6,14 +6,15 @@ const AnimalCard = ({ animal }) => {
   const navigate = useNavigate();
   const { selectAnimal } = useAnimal();
   return (
-    <div
-      className="animal-circle"
-      onClick={() => selectAnimal(animal.id)} 
-    >
-      <img src={animal.image ? animal.image : 'https://placehold.co/100'} alt={animal.name} />
-      <span className="animal-name">{animal.name}</span>
+    <div className="animal-card" onClick={() => selectAnimal(animal.id)}>
+      <div className="animal-circle">
+        <img
+          src={animal.image ? animal.image : "https://placehold.co/100"}
+          alt={animal.name}
+        />
+      </div>
+      <span className="animal-name">{animal.name}</span> {/* Dodany napis */}
     </div>
   );
-}
-
+};
 export default AnimalCard;
