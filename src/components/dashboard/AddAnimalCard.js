2bc9bfde57d1c915a16ca33249b06155f873e9df
Wegles <kuba2255kuba@gmail.com>
2024-12-15 16:46:12 +0100

    c

--- src/components/dashboard/AddAnimalCard.js
+++ src/components/dashboard/AddAnimalCard.js
@@ -2,7 +2,6 @@ import "../../styles/dashboard/AddAnimalCard.css";
 import { useNavigate } from "react-router-dom";
 
 const AddAnimalCard = () => {
-<<<<<<< HEAD
   const navigate = useNavigate();
   return (
     <div
@@ -13,17 +12,5 @@ const AddAnimalCard = () => {
     </div>
   );
 };
-=======
-    const navigate = useNavigate();
-    return (
-        <div
-            className="animal-circle add-animal"
-            onClick={() => navigate('/add-animal')} 
-        >
-            <span className="add-icon">+</span>
-        </div>
-    )
-}
->>>>>>> 264d67292a04c3ec8cf9556f5965e758537efa43
 
 export default AddAnimalCard;
